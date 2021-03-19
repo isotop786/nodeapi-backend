97ee0adac6949a4f280777834dff3f8f227ba74c
maruf786 <maruf@modello.my>
2021-03-20 02:02:15 +0600

    Post Model Redefine/Update

--- models/postModel.js
+++ models/postModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema;
 
 const postSchema = new mongoose.Schema({
     title:{
@@ -14,6 +15,18 @@ const postSchema = new mongoose.Schema({
         required:true,
         min:4,
         max:3000
+    },
+    photo:{
+        data:Buffer,
+        contenType: String
+    },
+    postedBy:{
+        type:ObjectId,
+        ref:"User"
+    },
+    created:{
+        type:Date,
+        default: Date.now
     }
 });
 
